bbe227d466a50e8f157dc968f0d2da987d40c83e
omar.khan2 <omar.khan2@cs.ucalgary.ca>
2022-03-23 15:57:57 -0600

    Completed methods of Nutrient class

--- src/Nutrient.java
+++ src/Nutrient.java
@@ -1,2 +1,19 @@
 public class Nutrient {
+
+    private String name;
+    private double target;
+    private String unit;
+
+    public String getName() {
+        return this.name;
+    }
+
+    public double getTarget() {
+        return this.target;
+    }
+
+    public String getUnit() {
+        return this.unit;
+    }
+
 }
