2aba1524e4a9d3438a86803834f91f689d1e90ac
Carter Tam <carter.tam@ucalgary.ca>
2022-03-23 14:54:17 -0600

    Added code to EntryTable.java

--- src/Entry.java
+++ src/Entry.java
@@ -19,16 +19,20 @@ public class Entry implements Comparator<Entry>{
         return this.nutrient.getName();
     }
 
+    public String getNutrientUnit(){
+        return this.nutrient.getUnit();
+    }
+
     public double getConsumption(){
         return this.getConsumption();
     }
 
-    public String getSurplusDefficiency() {
+    public String getSurplusDeficiency() {
         if(this.consumption > this.nutrient.getTarget()){ //determines if a surplus or deficiency is present on average, or is the user has meets their daily target on a given day
             return ("You consumed a surplus of " + this.nutrient.getName() + " by " + (this.consumption - this.nutrient.getTarget()) + " units based on your target goal on day " + this.day);
         }
         else if(this.consumption < this.nutrient.getTarget()){
-            return ("You were deficient in " + this.nutrient.getName() + " by " + (this.nutrient.getTarget() - this.consumption) + " units based on your target goal on day " + this.day);
+            return ("You were deficient in " + this.nutrient.getName() + " by " + (this.nutrient.getTarget() - this.consumption) + " " + this.nutrient.getUnit() + " based on your target goal on day " + this.day);
         }
         return null;
     }
--- src/EntryTable.java
+++ src/EntryTable.java
@@ -1,4 +1,6 @@
 import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
 
 public class EntryTable {
     private ArrayList<Entry> inputs;
@@ -6,4 +8,22 @@ public class EntryTable {
     public EntryTable (ArrayList<Entry> inputs){
         this.inputs = inputs;
     }
+
+    public String createTable(){
+        String table = "----------------------------------------------------------\n";
+        table = table + "DAY | NUTRIENT | QUANTITY\n";
+
+        Collections.sort(this.inputs, new Comparator<Entry>(){ //compares the items at index 0, which holds the 'day' value, to sort a list of string arrays containing the day, nutrient, and consumption values
+            public int compare(Entry ent1, Entry ent2){
+                return Integer.compare(ent1.getDay(), ent2.getDay());
+            }
+        });
+
+        for (Entry entry: this.inputs){ //after sorting the list, it's contents are semi-organized and printed to the console using a for loop
+            table = table + entry.getDay() + " | " + entry.getNutrientName() + " | " + entry.getConsumption() + " " + entry.getNutrientUnit() + "\n";
+        }
+        table = table + "----------------------------------------------------------";
+        return table;
+    }
+
 }
