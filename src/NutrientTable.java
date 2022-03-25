8ebcd354d211814d204c3e11592a8d5c5d37dc2c
omar.khan2 <omar.khan2@cs.ucalgary.ca>
2022-03-24 19:38:32 -0600

    Commented NutrientTable class

--- src/NutrientTable.java
+++ src/NutrientTable.java
@@ -8,11 +8,18 @@ public class NutrientTable {
         this.inputs = inputs;
     }
 
+    /**
+     * Creates the nutrient table with an entry for each Nutrient object in the "inputs" Arraylist
+     * @return the nutrient table in String form
+     */
     public String createTable() {
+        // an object which is used to construct a String
         StringBuilder tableBuilder = new StringBuilder();
+
         tableBuilder.append("----------------------------------------------------------\n");
         tableBuilder.append("NUTRIENT | UNIT | TARGET\n");
 
+        // loops each Nutrient object in the inputs Arraylist and adds the data to the StringBuilder
         for (Nutrient nutrient : inputs) {
             String nutrientName = nutrient.getName();
             String nutrientUnit = nutrient.getUnit();
@@ -23,6 +30,7 @@ public class NutrientTable {
         return tableBuilder.toString();
     }
 
+    // replaces the output of the default toString() function with the nutrient table
     @Override
     public String toString() {
         return createTable();
